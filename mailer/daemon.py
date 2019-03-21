839054bc9f2f9862ec7c8e4cfa138182cc427bb7
linuxdaemon <linuxdaemon@snoonet.org>
2019-03-21 09:46:54 -0500

    Refactor email handling logic
    Handle emails iteratively, making sure to wait before trying failing
    emails again

--- mailer/daemon.py
+++ mailer/daemon.py
@@ -5,6 +5,7 @@ from typing import Union
 
 import requests
 
+from mailer.message import UserEmail
 from .config import SMTPConfig, Config
 
 
@@ -24,12 +25,10 @@ class Daemon:
         if self.error_reporter:
             self.error_reporter.report_error(traceback.format_exc())
 
-    def get_email_file(self):
+    def get_email_files(self):
         files = list(self.mail_dir.rglob('*.mail'))
-        if not files:
-            return None
-
-        return random.choice(files)
+        random.shuffle(files)
+        return files
 
     def get_network(self, nick):
         server = self.get_reg_server(nick)
@@ -64,3 +63,16 @@ class Daemon:
 
     def tick(self):
         self.error_reporter.tick()
+
+    def handle_email(self, file):
+        email = UserEmail.from_file(file)
+        net = self.get_network(email.user)
+        if not net:
+            self.error_reporter.report_warning(
+                "Unable to determine network for user {!r}".format(email.user)
+            )
+            return False
+
+        net.send_email(email)
+        file.unlink()
+        return True
